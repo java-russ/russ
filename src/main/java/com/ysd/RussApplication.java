83377a263995479843f952c10ee5da10c39cd19c
yuezhiwen <50037945+yuezhiwen@users.noreply.github.com>
2019-10-11 09:24:29 +0800

    2332

--- src/main/java/com/ysd/RussApplication.java
+++ src/main/java/com/ysd/RussApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories("com.ysd.repository")
 public class RussApplication {
-
+//12
 	public static void main(String[] args) {
 		SpringApplication.run(RussApplication.class, args);
 	}
